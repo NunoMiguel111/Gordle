76fab92010ba5d696359d799167d7c3e3f0e3041
Nuno Miguel <nunomiguelitos14@gmail.com>
2023-02-06 17:35:22 +0000

    first commit: project from book'Learn go with pocket sized projects'

---
+++ gordle/game.go
@@ -0,0 +1,20 @@
+package gordle
+
+import "fmt"
+
+// Game holds all the information we need to play a game
+type Game struct{}
+
+// New returns a Game, which can be used to Play!
+func New() *Game {
+	g := &Game{}
+
+	return g
+}
+
+// Play runs the game.
+func (g *Game) Play() {
+	fmt.Println("Welcome to Gordle!")
+
+	fmt.Printf("Enter a guess:\n")
+}
